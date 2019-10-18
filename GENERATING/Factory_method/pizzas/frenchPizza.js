31e395c9640a00d7cf2c9ef3a3217e2b4f608d74
Daniil <Daniil.Yakovenko@artezio.com>
2019-10-18 18:15:00 +0300

    fix: fixed factory method pattern each pizza must take after parent pizza class!

--- GENERATING/Factory_method/pizzas/frenchPizza.js
+++ GENERATING/Factory_method/pizzas/frenchPizza.js
@@ -1,8 +1,14 @@
 'use strict';
+const Pizza = require('./pizza');
 
-module.exports = class FrenchPizza {
+module.exports = class FrenchPizza extends Pizza {
     constructor() {
+        super();
         this.price = 11;
         this.name = 'French';
+    }
+
+    smell() {
+        return 'Sharp pizza smell';
     }
 }
--- GENERATING/Factory_method/pizzas/peperoniPizza.js
+++ GENERATING/Factory_method/pizzas/peperoniPizza.js
@@ -1,8 +1,14 @@
 'use strict';
+const Pizza = require('./pizza');
 
-module.exports = class PeperoniPizza {
+module.exports = class PeperoniPizza extends Pizza {
     constructor() {
+        super();
         this.price = 10;
         this.name = 'Peperoni';
+    }
+
+    smell() {
+        return 'Tasty pizza smell';
     }
 }
